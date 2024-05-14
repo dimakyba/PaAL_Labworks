f7e61b6a1de40e2135998984af565cc2e5a0f3fb
Dmytro Kyba <dkyba2006@gmail.com>
2024-05-14 10:06:04 +0300

    i wanna drop computer science

--- 2nd_semester/Lab1/farbitre/d_category (quickSort)/StudSort.cs
+++ 2nd_semester/Lab1/farbitre/d_category (quickSort)/StudSort.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.AccessControl;
 using System.Text;
+using System.Threading;
 
 #if VALUE_IS_DOUBLE
-	using ValueType = System.Double;
+using ValueType = System.Double;
 #else
 #if VALUE_IS_SHORT
 	using ValueType = System.Int16;
@@ -17,63 +20,39 @@ using ValueType = System.Int32;
 #endif
 #endif
 
-using System;
-
-using System;
-
 namespace SortTest
 {
-  class StudSort
-  {
-    // You may add more method(s) here, if needed
-    // (f.e., for recursive quicksort or recursive mergesort)
-
-    public static ValueType[] Sort(ValueType[] data) // DON'T CHANGE this line!!!
-    {
-      QuickSort(data, 0, data.Length - 1);
-
-      // Add code which actually sorts.
-      // Use int type for indice and ValueType for values in array.
-      // If you change ValueType[] data (it IS allowed andf weakly recommended),
-      // just leave "return data" as is.
-      // If you create another array with sorted data, change "return data" with returning of that new array
-      return data;
-    }
-
-    public static void QuickSort(ValueType[] arr, int low, int high)
-    {
-      if (low < high)
-      {
-        int pi = Partition(arr, low, high);
-
-        QuickSort(arr, low, pi - 1);
-        QuickSort(arr, pi + 1, high);
-      }
-    }
-
-    public static int Partition(ValueType[] arr, int low, int high)
-    {
-      ValueType pivot = arr[high];
-      int i = (low - 1);
-
-      for (int j = low; j < high; j++)
-      {
-        if (arr[j] < pivot)
-        {
-          i++;
-          ValueType temp = arr[i];
-          arr[i] = arr[j];
-          arr[j] = temp;
-        }
-      }
-
-      int temp1 = arr[i + 1];
-      arr[i + 1] = arr[high];
-      arr[high] = temp1;
-
-      return i + 1;
-    }
-
-
-  }
+	class StudSort
+	{
+		// You may add more method(s) here, if needed
+		// (f.e., for recursive quicksort or recursive mergesort)
+		static void Swap(ref ValueType a, ref ValueType b)
+		{
+			var temp = a; a = b; b = temp;
+		}
+		static void QuickSort(ValueType[] array, int left, int right)
+		{
+			var pivot = array[(right + left) / 2];
+			int i = left; int j = right;
+			do
+			{
+				while (array[i] < pivot) i++;
+				while (array[j] > pivot) j--;
+				if (i <= j)
+				{
+					Swap(ref array[i], ref array[j]);
+					i++;
+					j--;
+				}
+			}
+			while (i < j);
+			if (j > left) QuickSort(array, left, j);
+			if (i < right) QuickSort(array, i, right);
+		}
+		public static ValueType[] Sort(ValueType[] data) // DON'T CHANGE this line!!!
+		{
+			QuickSort(data, 0, data.Length - 1);
+			return data;
+		}
+	}
 }
